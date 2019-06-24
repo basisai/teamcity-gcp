a56a935a66842bc702b5ee95133a54c246ab1dc9
Yong Wen Chua <lawliet89@users.noreply.github.com>
2019-06-24 10:04:03 +0800

    Upgrade Agent to Terraform 0.12 (#6)

--- agent/main.tf
+++ agent/main.tf
@@ -1,5 +1,8 @@
 locals {
-  instance_image = "${coalesce(var.instance_image, data.google_compute_image.agent.self_link)}"
+  instance_image = coalesce(
+    var.instance_image,
+    data.google_compute_image.agent.self_link,
+  )
 
   # Roles for the Server service account
   # c.f. https://blog.jetbrains.com/teamcity/2017/06/run-teamcity-ci-builds-in-google-cloud/
@@ -11,68 +14,67 @@ locals {
 }
 
 resource "google_service_account" "agent" {
-  account_id   = "${var.service_account_name}"
-  display_name = "${var.service_account_display}"
+  account_id   = var.service_account_name
+  display_name = var.service_account_display
 
-  project = "${var.project_id}"
+  project = var.project_id
 }
 
 resource "google_service_account" "server" {
-  count = "${var.server_service_account_create ? 1 : 0}"
+  count = var.server_service_account_create ? 1 : 0
 
-  account_id   = "${var.server_service_account_name}"
-  display_name = "${var.server_service_account_display}"
+  account_id   = var.server_service_account_name
+  display_name = var.server_service_account_display
 
-  project = "${var.project_id}"
+  project = var.project_id
 }
 
 resource "google_project_iam_member" "server" {
-  count = "${var.server_service_account_create ? length(local.server_roles) : 0}"
+  count = var.server_service_account_create ? length(local.server_roles) : 0
 
-  role    = "${element(local.server_roles, count.index)}"
-  project = "${var.project_id}"
-  member  = "serviceAccount:${google_service_account.server.email}"
+  role    = element(local.server_roles, count.index)
+  project = var.project_id
+  member  = "serviceAccount:${google_service_account.server[0].email}"
 }
 
 data "google_compute_image" "agent" {
-  family  = "${var.image_family}"
-  project = "${var.project_id}"
+  family  = var.image_family
+  project = var.project_id
 }
 
 resource "google_compute_instance_template" "agent" {
-  count = "${var.instance_subnetworks_count}"
+  count = var.instance_subnetworks_count
 
-  project = "${var.project_id}"
-  region  = "${var.region}"
+  project = var.project_id
+  region  = var.region
 
-  name_prefix          = "${var.instance_template_prefix}"
-  description          = "${var.instance_template_description}"
-  instance_description = "${var.instance_description}"
+  name_prefix          = var.instance_template_prefix
+  description          = var.instance_template_description
+  instance_description = var.instance_description
 
-  machine_type            = "${var.machine_type}"
-  labels                  = "${var.labels}"
-  metadata                = "${var.metadata}"
-  metadata_startup_script = "${var.metadata_startup_script}"
-  tags                    = "${var.tags}"
+  machine_type            = var.machine_type
+  labels                  = var.labels
+  metadata                = var.metadata
+  metadata_startup_script = var.metadata_startup_script
+  tags                    = var.tags
 
   disk {
     boot         = true
     auto_delete  = true
-    source_image = "${local.instance_image}"
-    disk_type    = "${var.disk_type}"
-    disk_size_gb = "${var.disk_size_gb}"
+    source_image = local.instance_image
+    disk_type    = var.disk_type
+    disk_size_gb = var.disk_size_gb
   }
 
   network_interface {
-    subnetwork         = "${element(var.instance_subnetworks, count.index)}"
-    subnetwork_project = "${element(var.instance_subnetworks_projects, count.index)}"
-
+    subnetwork         = element(var.instance_subnetworks, count.index)
+    subnetwork_project = element(var.instance_subnetworks_projects, count.index)
     # No external IP for now
     # access_config {}
   }
 
   service_account {
-    email = "${google_service_account.agent.email}"
+    email = google_service_account.agent.email
 
     scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
--- agent/outputs.tf
+++ agent/outputs.tf
@@ -1,24 +1,24 @@
 output "service_account_name" {
   description = "Service Account name for TeamCity agent"
-  value       = "${google_service_account.agent.name}"
+  value       = google_service_account.agent.name
 }
 
 output "service_account_email" {
   description = "Service Account email for TeamCity agent"
-  value       = "${google_service_account.agent.email}"
+  value       = google_service_account.agent.email
 }
 
 output "server_service_account_name" {
   description = "Service Account name for TeamCity server"
-  value       = "${google_service_account.server.name}"
+  value       = google_service_account.server[0].name
 }
 
 output "server_service_account_email" {
   description = "Service Account email for TeamCity server"
-  value       = "${google_service_account.server.email}"
+  value       = google_service_account.server[0].email
 }
 
 output "instance_template" {
   description = "Self-link to the instance template"
-  value       = "${google_compute_instance_template.agent.self_link}"
+  value       = google_compute_instance_template.agent[0].self_link
 }

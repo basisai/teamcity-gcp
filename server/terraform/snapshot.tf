d1ee814d97d9e58ef188a0744eaa05a8e8d29e17
Chris Sng <chrissng@users.noreply.github.com>
2019-09-25 12:21:58 +0800

    Attach snapshot policy to backup TC server data disk periodically (#14)
    * Attach snapschot schedule policy to disk
    
    * use beta
    
    * fix cycle, update README
    
    * resource policies attachment
    
    * terraform-provider-google 2.16

--- server/terraform/snapshot.tf
+++ server/terraform/snapshot.tf
@@ -1,32 +1,8 @@
 locals {
-  policy_name = "scheduled-snapshot-for-${google_compute_disk.teamcity_server_data.name}"
-}
-
-resource "null_resource" "unattach_policy" {
-  # Changes to any instance of the cluster requires re-provisioning
-  triggers = {
-    name               = local.policy_name
-    project            = var.project_id
-    region             = var.region
-    max_retention_days = var.max_retention_days
-    days_in_cycle      = var.snapshot_days_in_cycle
-    start_time         = var.snapshot_start_time
-  }
-
-  provisioner "local-exec" {
-    command = "gcloud beta compute disks remove-resource-policies $DISK_NAME --resource-policies $SCHEDULE_NAME --zone $ZONE || true"
-
-    environment = {
-      DISK_NAME     = google_compute_disk.teamcity_server_data.self_link
-      SCHEDULE_NAME = local.policy_name
-      ZONE          = google_compute_disk.teamcity_server_data.zone
-    }
-  }
+  policy_name = "scheduled-snapshot-for-${var.data_disk_name}"
 }
 
 resource "google_compute_resource_policy" "teamcity_server_data" {
-  provider = "google-beta"
-
   name    = local.policy_name
   project = var.project_id
   region  = var.region
@@ -43,19 +19,16 @@ resource "google_compute_resource_policy" "teamcity_server_data" {
       on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
     }
     snapshot_properties {
-      labels            = merge(var.labels, { checksum = null_resource.unattach_policy.id })
+      labels            = var.labels
       storage_locations = [var.region]
       guest_flush       = false
     }
   }
+}
 
-  provisioner "local-exec" {
-    command = "gcloud beta compute disks add-resource-policies $DISK_NAME --resource-policies $SCHEDULE_NAME --zone $ZONE"
-
-    environment = {
-      DISK_NAME     = google_compute_disk.teamcity_server_data.self_link
-      SCHEDULE_NAME = local.policy_name
-      ZONE          = google_compute_disk.teamcity_server_data.zone
-    }
-  }
+resource "google_compute_disk_resource_policy_attachment" "teamcity_server_data" {
+  zone    = var.zone
+  project = var.project_id
+  disk    = google_compute_disk.teamcity_server_data.name
+  name    = google_compute_resource_policy.teamcity_server_data.name
 }
